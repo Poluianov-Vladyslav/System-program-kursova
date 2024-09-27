f258cd6cce213fda63549830a79cfb4e54db5aa0
Poluianov <151568370+Poluianov-Vladyslav@users.noreply.github.com>
2024-09-27 19:11:22 +0300

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,17 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+    public:
+	HEAD
+        int Add (double, double);
+        int Sub (double, double);
+	
+        int Add (int, int);
+        int Mul (double, double);
+	e0ea21b (add a multiplication operation)
+	void PrintResult(double a, double b);
+};
+
+#endif//CALCULATOR_H
